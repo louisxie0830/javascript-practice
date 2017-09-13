5552b0313ce8ad5710298f4a91ba076da5629443
louisxie <louisxie@truetel.com>
2017-09-13 12:10:35 +0800

    add unit.js

---
+++ js/unit.js
@@ -0,0 +1,237 @@
+function trim(str, type) {
+    switch (type) {
+        case 1: return str.replace(/\s+/g, "");
+        case 2: return str.replace(/(^\s*)|(\s*$)/g, "");
+        case 3: return str.replace(/(^\s*)/g, "");
+        case 4: return str.replace(/(\s*$)/g, "");
+        default: return str;
+    }
+}
+
+function changeCase(str, type) {
+    const ToggleCase = (str) => {
+        var itemText = ""
+        str.split("").forEach(
+            function (item) {
+                if (/^([a-z]+)/.test(item)) {
+                    itemText += item.toUpperCase();
+                }
+                else if (/^([A-Z]+)/.test(item)) {
+                    itemText += item.toLowerCase();
+                }
+                else {
+                    itemText += item;
+                }
+            });
+        return itemText;
+    };
+
+    switch (type) {
+        case 1:
+            return str.replace(/^(\w)(\w+)/, function (v, v1, v2) {
+                return v1.toUpperCase() + v2.toLowerCase();
+            });
+        case 2:
+            return str.replace(/^(\w)(\w+)/, function (v, v1, v2) {
+                return v1.toLowerCase() + v2.toUpperCase();
+            });
+        case 3:
+            return ToggleCase(str);
+        case 4:
+            return str.toUpperCase();
+        case 5:
+            return str.toLowerCase();
+        default:
+            return str;
+    }
+}
+
+function repeatStr(str, count) {
+    var text = '';
+    for (var i = 0; i < count; i++) {
+        text += str;
+    }
+    return text;
+}
+
+
+function replaceAll(str, AFindText, ARepText) {
+    raRegExp = new RegExp(AFindText, "g");
+    return str.replace(raRegExp, ARepText);
+}
+
+
+
+function replaceAll(str, AFindText, ARepText) {
+    raRegExp = new RegExp(AFindText, "g");
+    return str.replace(raRegExp, ARepText);
+}
+
+
+function replaceStr(str, regArr, type, ARepText) {
+    var regtext = '', Reg = null, replaceText = ARepText || '*';
+
+    if (regArr.length === 3 && type === 0) {
+        regtext = '(\\w{' + regArr[0] + '})\\w{' + regArr[1] + '}(\\w{' + regArr[2] + '})'
+        Reg = new RegExp(regtext);
+        var replaceCount = repeatStr(replaceText, regArr[1]);
+        return str.replace(Reg, '$1' + replaceCount + '$2')
+    }
+
+    else if (regArr.length === 3 && type === 1) {
+        regtext = '\\w{' + regArr[0] + '}(\\w{' + regArr[1] + '})\\w{' + regArr[2] + '}'
+        Reg = new RegExp(regtext);
+        var replaceCount1 = repeatSte(replaceText, regArr[0]);
+        var replaceCount2 = repeatSte(replaceText, regArr[2]);
+        return str.replace(Reg, replaceCount1 + '$1' + replaceCount2)
+    }
+
+    else if (regArr.length === 1 && type == 0) {
+        regtext = '(^\\w{' + regArr[0] + '})'
+        Reg = new RegExp(regtext);
+        var replaceCount = repeatSte(replaceText, regArr[0]);
+        return str.replace(Reg, replaceCount)
+    }
+
+    else if (regArr.length === 1 && type == 1) {
+        regtext = '(\\w{' + regArr[0] + '}$)'
+        Reg = new RegExp(regtext);
+        var replaceCount = repeatSte(replaceText, regArr[0]);
+        return str.replace(Reg, replaceCount)
+    }
+}
+
+
+function checkType(str, type) {
+    switch (type) {
+        case 'email':
+            return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(str);
+        case 'phone':
+            return /^1[3|4|5|7|8][0-9]{9}$/.test(str);
+        case 'tel':
+            return /^(0\d{2,3}-\d{7,8})(-\d{1,4})?$/.test(str);
+        case 'number':
+            return /^[0-9]$/.test(str);
+        case 'english':
+            return /^[a-zA-Z]+$/.test(str);
+        case 'chinese':
+            return /^[\u4E00-\u9FA5]+$/.test(str);
+        case 'lower':
+            return /^[a-z]+$/.test(str);
+        case 'upper':
+            return /^[A-Z]+$/.test(str);
+        default:
+            return true;
+    }
+}
+
+
+function checkPwd(str) {
+    var nowLv = 0;
+    if (str.length < 6) {
+        return nowLv
+    }
+    ;
+    if (/[0-9]/.test(str)) {
+        nowLv++
+    }
+    ;
+    if (/[a-z]/.test(str)) {
+        nowLv++
+    }
+    ;
+    if (/[A-Z]/.test(str)) {
+        nowLv++
+    }
+    ;
+    if (/[\.|-|_]/.test(str)) {
+        nowLv++
+    }
+    ;
+    return nowLv;
+}
+
+
+
+
+function randomNumber(count) {
+    return Math.random().toString(count).substring(2);
+}
+
+function countStr(str, strSplit) {
+    return str.split(strSplit).length - 1
+}
+var strTest = 'sad44654blog5a1sd67as9dablog4s5d16zxc4sdweasjkblogwqepaskdkblogahseiuadbhjcibloguyeajzxkcabloguyiwezxc967'
+
+function removeRepeatArray(arr) {
+    return Array.from(new Set(arr))
+}
+
+
+function upsetArr(arr) {
+    return arr.sort(function () { return Math.random() - 0.5 });
+}
+
+
+
+function maxArr(arr) {
+    return Math.max.apply(null, arr);
+}
+function minArr(arr) {
+    return Math.min.apply(null, arr);
+}
+
+
+
+function sumArr(arr) {
+    var sumText = 0;
+    for (var i = 0, len = arr.length; i < len; i++) {
+        sumText += arr[i];
+    }
+    return sumText
+}
+
+function covArr(arr) {
+    var sumText = sumArr(arr);
+    var covText = sumText / length;
+    return covText
+}
+
+function randomOne(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function getEleCount(obj, ele) {
+    var num = 0;
+    for (var i = 0, len = obj.length; i < len; i++) {
+        if (ele == obj[i]) {
+            num++;
+        }
+    }
+    return num;
+}
+
+function getCount(arr, rank, ranktype) {
+
+    var obj = {}, k, arr1 = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+        k = arr[i];
+        if (obj[k]) {
+            obj[k]++;
+        }
+        else {
+            obj[k] = 1;
+        }
+    }
+    for (var o in obj) {
+        arr1.push({ el: o, count: obj[o] });
+    }
+    arr1.sort(function (n1, n2) {
+        return n2.count - n1.count
+    });
+    if (ranktype === 1) {
+        arr1 = arr1.reverse();
+    }
+    var rank1 = rank || arr1.length;
+    return arr1.slice(0, rank1);
+}
