6ef257b72f6364dd7ec448ae12d5eb847ac6901f
louisxie <louisxie@truetel.com>
2017-09-26 16:50:51 +0800

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp'),
     gulpSass = require('gulp-sass'),
-    gulpPlumber = require('gulp-plumber');
+    gulpPlumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer');
 
 
 // build scss   
 gulp.task('style', () => {
     gulp.src('css/**.scss')
         .pipe(gulpPlumber())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulpSass({
             outputStyle: 'compressed'
         }))
